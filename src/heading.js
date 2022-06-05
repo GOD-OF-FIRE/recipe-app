fa11442ef19caeafb6c112e2b5b6f6747066d3a1
Kushagra Gupta <kushagra357@gmail.com>
2022-06-05 11:58:15 +0530

    test 05062022

---
+++ src/heading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Heading = () =>{
+    return(
+        <div className="heading">
+            <h1>
+                Food Recipe App
+            </h1>
+            <p>Kushagra Gupta</p>
+        </div>
+    )
+}
+
+export default Heading;
